cf88d0caf83befc28d26e8334342dd9be3154baa
vagrant <vagrant@homestead>
2020-05-04 02:38:09 +0000

    tests

---
+++ tests/Gateways/AlibabaCloudGatewayTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Strayjoke\Dogsms\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class AlibabaCloudGatewayTest extends TestCase
+{
+    public function testSendSms()
+    {
+    }
+}
---
+++ tests/SmsManagerTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Strayjoke\Dogsms\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Strayjoke\Dogsms\SmsManager;
+use Strayjoke\Dogsms\Gateways\AlibabaCloudGateway;
+
+class SmsManagerTest extends TestCase
+{
+    public function testSortGateways()
+    {
+        $manager = new SmsManager(['default' => ['gateways' => ['alibaba', 'tencent']]]);
+        $tempArr = $manager->sortGateways();
+        $this->assertCount(2, $tempArr);
+        $this->assertCount(2, $tempArr);
+        $this->assertContains('alibaba', $tempArr);
+        $this->assertContains('tencent', $tempArr);
+    }
+
+    public function testGateway()
+    {
+        $manager = new SmsManager([
+            'default' => [
+                'gateways' => ['alibabaCloud']
+            ],
+            'gateways' => [
+                'alibabaCloud' => [
+                    'access_key_id' => 'mock-id',
+                    'access_key_secret' => 'mock-secret',
+                    'sign_name' => 'mock-sign',
+                ]
+            ]
+        ]);
+        $tempGateway = $manager->gateway('alibabaCloud');
+        $this->assertInstanceOf(AlibabaCloudGateway::class, $tempGateway);
+    }
+}
